bb3dd5979b16488914075a5e6607cb7fac362e9c
Eduard.Rosu <eduad.rosu@vivre-adms-mbp.home>
2020-08-25 22:57:30 +0300

    finish quizz

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -5,21 +5,51 @@
                     {{ currentQuestion.question }}
                 </template>
                 <hr class="my-4">
-                <p v-for="(answer, index) in answers" :key="index">
-                    {{answer}}
-                </p>
 
-                <b-button variant="primary" href="#">Submit</b-button>
-                <b-button @click="next" variant="success" href="#">Next</b-button>
+                <b-list-group>
+                  <b-list-group-item
+                    v-for="(answer, index) in answers"
+                    :key="index"
+                    :class="answerClass(index)"
+                    @click="selectAnswer(index)"
+                  >
+                    {{answer}}
+                  </b-list-group-item>
+                </b-list-group>
+
+                <b-button
+                 variant="primary"
+                 @click="submitAnswer()"
+                 :disabled="selectedIndex === null || answered"
+                >Submit
+                </b-button>
+                <b-button
+                  variant="success"
+                  @click="next"
+                >
+                Next
+                </b-button>
             </b-jumbotron>
     </div>
 </template>
 
 <script>
+
+import _ from 'lodash'
+
 export default {
   props: {
     currentQuestion: Object,
-    next: Function
+    next: Function,
+    increment: Function
+  },
+  data () {
+    return {
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
+    }
   },
   computed: {
     answers () {
@@ -28,8 +58,68 @@ export default {
       return answers
     }
   },
-  mounted () {
-    console.log(this.currentQuestion)
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler () {
+        this.selectedIndex = null
+        this.answered = false
+        this.shuffleAnswers()
+      }
+    }
+  },
+  methods: {
+    selectAnswer (index) {
+      this.selectedIndex = index
+    },
+    submitAnswer () {
+      let isCorrect = false
+      if (this.selectedIndex === this.correctIndex) {
+        isCorrect = true
+      }
+
+      this.answered = true
+      this.increment(isCorrect)
+    },
+    answerClass (index) {
+      let answerClass = ''
+
+      if (!this.answered && this.selectedIndex === index) {
+        answerClass = 'selected'
+      } else if (this.answered && this.correctIndex === index) {
+        answerClass = 'correct'
+      } else if (this.answered && this.correctIndex !== index && this.selectedIndex === index) {
+        answerClass = 'incorrect'
+      }
+
+      return answerClass
+    },
+    shuffleAnswers () {
+      const answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+      this.shuffledAnswers = _.shuffle(answers)
+      this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+    }
   }
 }
 </script>
+
+<style scoped>
+  .list-group {
+    margin-bottom: 15px;
+  }
+  .list-group-item:hover {
+    background: #EEE;
+  }
+  .btn {
+    margin: 0 5px;
+  }
+  .selected {
+    background-color: lightblue;
+  }
+  .correct {
+    background-color: lightgreen;
+  }
+  .incorrect {
+    background-color: red;
+  }
+</style>
