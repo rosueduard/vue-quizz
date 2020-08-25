12a8a99402a66330e38ba914a3ef293e91f4008a
Eduard.Rosu <eduad.rosu@vivre-adms-mbp.home>
2020-08-25 16:57:52 +0300

    add vue boostrap

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav>
+            <b-nav-item active>Fancy Quizz App</b-nav-item>
+            <b-nav-item>Counter: </b-nav-item>
+        </b-nav>
+    </div>
+</template>
